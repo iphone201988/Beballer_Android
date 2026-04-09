7f75e80960bb8f87d10fb1dedd429c8405056206
iphone201988 <iphone201988@gmail.com>
2026-04-09 10:41:38 +0530

    git_push_issue fix

--- app/src/main/java/com/beballer/beballer/ui/player/dash_board/find/map/ShowMapFragment.kt
+++ app/src/main/java/com/beballer/beballer/ui/player/dash_board/find/map/ShowMapFragment.kt
@@ -8,7 +8,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Handler
-
 import android.view.View
 import android.view.animation.OvershootInterpolator
 import androidx.appcompat.widget.SearchView
